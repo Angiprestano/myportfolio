ad1b3ebd77645065bcddacb8026714fa35b09207
Angela Prestano <angiprestano99@gmail.com>
2024-08-24 17:05:58 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Intro from "./Intro";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
